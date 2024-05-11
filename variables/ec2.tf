1bfbebe988176b4f4bdbcede77af55bee159ea63
AnilKManthena <akmanthena@gmail.com>
2024-05-12 00:16:54 +0530

    terraform-ec2-practice

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "db" {
 
     ami = var.image_id
-    vpc_security_group_ids = [aws_securitygroup.allow_ssh.id]
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
     instance_type = var.instance_type
     # left side things are called as arguments, right side are values.
     tags = var.tags
