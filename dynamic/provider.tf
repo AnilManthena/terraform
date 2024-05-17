f90cd3e4287eb3f77b50b0340f3ca6954470b7c9
AnilKManthena <akmanthena@gmail.com>
2024-05-17 20:06:26 +0530

    terraform-dynamic-practice

---
+++ dynamic/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.49.0"
+    }
+  }
+  backend "s3" {    
+    bucket = "anilk-s3bucket"    
+    key    = "dynamic-demo"    
+    region = "us-east-1" 
+    dynamodb_table = "devops4me-locking"
+  }
+}
+
+#provide authentication here
+provider "aws" {
+  region = "us-east-1"
+}
